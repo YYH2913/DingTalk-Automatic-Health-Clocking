dd33e7d7c8b14089074f22942a7b23bfdba820e6
YYH2913 <77504776+YYH2913@users.noreply.github.com>
2021-01-16 19:48:19 +0800

    Add files via upload

---
+++ mod_config.py
@@ -0,0 +1,12 @@
+#encoding:utf-8
+#name:mod_config.py
+
+import configparser
+import os
+
+#获取config配置文件
+def getConfig(section, key):
+    config = configparser.ConfigParser()
+    path = os.path.split(os.path.realpath(__file__))[0] + '/db.conf'
+    config.read(path)
+    return config.get(section, key)
